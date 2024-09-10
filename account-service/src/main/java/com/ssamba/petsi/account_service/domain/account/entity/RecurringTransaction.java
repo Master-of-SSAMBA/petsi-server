8122c0a3bcc5e9300599d72c2df9816eec1bf207
조수연 <melthetensai@gmail.com>
2024-09-10 10:01:26 +0900

    Refactor: change type of status in RecurringTransaction Entity
    1. status type: boolean to String

--- account-service/src/main/java/com/ssamba/petsi/account_service/domain/account/entity/RecurringTransaction.java
+++ account-service/src/main/java/com/ssamba/petsi/account_service/domain/account/entity/RecurringTransaction.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import com.ssamba.petsi.account_service.domain.account.enums.AccountStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -42,7 +44,7 @@ public class RecurringTransaction {
 	private LocalDateTime nextTransactionDate;
 
 	@Column(nullable = false)
-	private boolean status;
+	private String status;
 	@CreatedDate
 	private LocalDateTime createdAt;
 	@LastModifiedDate
@@ -52,7 +54,7 @@ public class RecurringTransaction {
 	protected void onCreate() {
 		this.createdAt = LocalDateTime.now();
 		this.updatedAt = LocalDateTime.now();
-		this.status = true;
+		this.status = AccountStatus.ACTIVATED.getValue();
 	}
 
 }
