cfbe9e568fb8dcc596534ee2c6940da36379c276
최이서 <chxiseo@naver.com>
2024-09-11 17:03:24 +0900

    Feat: 로그인 화면 구현

--- frontend/petsi-client/src/pages/login/Login.style.ts
+++ frontend/petsi-client/src/pages/login/Login.style.ts
@@ -1,7 +1,29 @@
 import styled from "styled-components";
 
-export const Message = styled.span`
+export const Layout = styled.div`
+    display: flex;
+    width: 85%;
+    margin: 0 auto;
+    flex-direction: column;
+    padding-top: 10%;
+    gap: 2rem;
+`;
+
+export const TitleMsg = styled.span`
     font-size: var(--font-title-h3);
-    margin-bottom: 10px;
     font-weight: 500;
+`;
+
+export const InputSection = styled.div`
+    text-align: end;
+`;
+
+export const BtnSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
+export const FindPassword = styled.p`
+    margin-top: 1rem;
 `;
