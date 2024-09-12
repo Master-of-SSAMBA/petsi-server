bb7219c79a1ef20329dc9fe432656ab3c5f77d37
liarreez <liarreez@gmail.com>
2024-09-12 20:38:19 +0900

    Fix 게이트웨이 서비스를 webflux 형태로 변경
    기존에 mvc 구조ë였던 게이트웨이 서비스를 webflux 형태로 변경

--- backend/gateway-service/src/main/java/com/ssamba/petsi/gateway_service/GatewayServiceApplication.java
+++ backend/gateway-service/src/main/java/com/ssamba/petsi/gateway_service/GatewayServiceApplication.java
@@ -2,8 +2,9 @@ package com.ssamba.petsi.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
