7332e9b78c4c22c685596f29bd0bdc6ebcfd7db4
최다환 <sinphoto@naver.com>
2024-09-12 10:02:18 +0900

    Feat: add picture feat

---
+++ backend/picture-service/src/main/java/com/ssamba/petsi/picture_service/domain/account/dto/response/PictureResponseDto.java
@@ -0,0 +1,34 @@
+package com.ssamba.petsi.picture_service.domain.account.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class PictureResponseDto {
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private Long pictureId;
+        private String img;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Detail {
+        private Long pictureId;
+        private String img;
+        private String content;
+        private DateResponseDto registDate;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Count {
+        private Long pictureCnt;
+    }
+
+}
