328db56a5240da07c8cd8c08b697ee1096c81bdf
최다환 <sinphoto@naver.com>
2024-09-26 17:17:12 +0900

    Refactor : python ai api 요청 url 주석 추가

--- backend/expense-service/src/main/java/com/ssamba/petsi/expense_service/domain/expense/service/ExpenseService.java
+++ backend/expense-service/src/main/java/com/ssamba/petsi/expense_service/domain/expense/service/ExpenseService.java
@@ -34,7 +34,8 @@ public class ExpenseService {
 
     private static final long EXPENSE_PER_PAGE = 20;
     private static final String CACHE_KEY_PREFIX = "expense:";
-    private static final String AI_PREDICT_URL = "http://127.0.0.1:8000/predict_categories";
+    // python AI 카테고리 예측 요청 URI
+    private static final String AI_PREDICT_URL = "http://127.0.0.1:8000/api/v1/predict_categories";
     private final RestTemplate restTemplate = new RestTemplate();
     private final S3Service s3Service;
     private final PurchaseRepository purchaseRepository;
