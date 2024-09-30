955e034ad559a4473ced0f1458661c2c4a64482c
조수연 <melthetensai@gmail.com>
2024-09-30 15:28:41 +0900

    Refactor: revert ScheduleStatus

--- backend/schedule-service/src/main/java/com/ssamba/petsi/schedule_service/domain/schedule/enums/ScheduleStatus.java
+++ backend/schedule-service/src/main/java/com/ssamba/petsi/schedule_service/domain/schedule/enums/ScheduleStatus.java
@@ -1,42 +1,16 @@
 package com.ssamba.petsi.schedule_service.domain.schedule.enums;
 
-import java.util.List;
-
-import com.ssamba.petsi.schedule_service.domain.schedule.dto.response.GetSchedulesDetailPerMonthResponseDto;
-import com.ssamba.petsi.schedule_service.global.dto.PetCustomDto;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public enum ScheduleStatus {
-	ACTIVATED("활성") {
-		@Override
-		public List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> setReturnList(List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> upcommingList,
-			List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> endedScheduleList) {
-			return List.of();
-		}
-	},
-	INACTIVATED("비활성") {
-		@Override
-		public List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> setReturnList(List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> upcommingList,
-			List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> endedScheduleList) {
-			return List.of();
-		}
-	},
-	ENDED("완료") {
-		@Override
-		public List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> setReturnList(List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> upcommingList,
-			List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> endedScheduleList) {
-			return List.of();
-		}
-	};
+	ACTIVATED("활성"),
+	INACTIVATED("비활성"),
+	ENDED("완료");
 
 	private final String value;
 
-
-	public abstract List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> setReturnList(
-		List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> upcommingList, List<GetSchedulesDetailPerMonthResponseDto<PetCustomDto>> endedScheduleList);
 }
 
