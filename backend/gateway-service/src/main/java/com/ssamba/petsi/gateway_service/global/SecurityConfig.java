a7d9a0f7c92e1991965fd2f737ca8f613a0bd05f
양지웅 <yju1028@naver.com>
2024-10-01 20:48:14 +0900

    Update SecurityConfig.java

--- backend/gateway-service/src/main/java/com/ssamba/petsi/gateway_service/global/SecurityConfig.java
+++ backend/gateway-service/src/main/java/com/ssamba/petsi/gateway_service/global/SecurityConfig.java
@@ -1,52 +1,42 @@
 package com.ssamba.petsi.gateway_service.global;
 
-import java.util.Arrays;
-import org.springframework.http.HttpMethod;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CorsSpec;
+import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
-        return http
-                .authorizeExchange(exchange -> exchange
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/api/**"))
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/api/v1/user/signup", "/api/v1/auth/**").permitAll()
+                        .pathMatchers("/api/v1/**").authenticated()
                         .anyExchange().permitAll()
                 )
-                .exceptionHandling(exceptionHandling -> exceptionHandling
-                        .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
-                // .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(ServerHttpSecurity.CorsSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .build();
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .csrf(CsrfSpec::disable)
+                .cors(CorsSpec::disable);
+
+        return http.build();
     }
 
     @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
-                "https://j11a403.p.ssafy.io",
-                "http://petsi-frontend-bucket.s3-website.ap-northeast-2.amazonaws.com"
-        ));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        configuration.setExposedHeaders(Arrays.asList("Authorization"));
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    SecurityWebFilterChain frontendSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .securityMatcher(new PathPatternParserServerWebExchangeMatcher("/api/**"))
+                .authorizeExchange(exchanges -> exchanges.anyExchange().permitAll())
+                .csrf(CsrfSpec::disable)
+                .cors(CorsSpec::disable);
+        return http.build();
     }
 }
