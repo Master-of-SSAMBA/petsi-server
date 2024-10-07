85567e3d60a487f768d1cc4e57d0685a26f7e117
양지웅 <yju1028@naver.com>
2024-10-07 21:33:10 +0900

    Feat:파이어베이스 설정

---
+++ backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/config/FirebaseConfig.java
@@ -0,0 +1,33 @@
+package com.ssamba.petsi.notice_service.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
+
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            InputStream serviceAccount =
+                    getClass().getResourceAsStream("/firebase-service-account.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        }
+        return FirebaseApp.getInstance();
+    }
+}
--- backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/kafka/KafkaConsumer.java
+++ backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/kafka/KafkaConsumer.java
@@ -1,4 +1,7 @@
 package com.ssamba.petsi.notice_service.global.kafka;
 
+import com.ssamba.petsi.notice_service.domain.notice.dto.response.NoticeResponseDto;
+import org.springframework.kafka.annotation.KafkaListener;
+
 public class KafkaConsumer {
 }
