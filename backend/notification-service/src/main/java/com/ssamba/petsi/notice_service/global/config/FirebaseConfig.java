78b47424dac22b02e5f95f4f7da60b0b9f76b0b9
양지웅 <yju1028@naver.com>
2024-10-07 21:44:13 +0900

    Update FirebaseConfig.java

--- backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/config/FirebaseConfig.java
+++ backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/config/FirebaseConfig.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
