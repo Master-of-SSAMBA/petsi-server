05cb21fab7e2d926afbe64c67b6f8c9658e50495
양지웅 <yju1028@naver.com>
2024-10-07 13:18:31 +0900

    Delete V2__dummy_data.sql

--- backend/pet-service/src/main/resources/db/migration/V2__dummy_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO pet (pet_id, user_id, name, birthdate, species, breed, gender, weight, status)
-VALUES (998, '단지', '2020-05-20', 'dog', '세상에 하나뿐인 믹스', 'male', 2.3, '활성');
-INSERT INTO pet (pet_id, user_id, name, birthdate, species, breed, gender, weight, status)
-VALUES (999, '홍시', '2020-05-20', 'cat', '코리안숏헤어', 'female', 5.6, '활성');
