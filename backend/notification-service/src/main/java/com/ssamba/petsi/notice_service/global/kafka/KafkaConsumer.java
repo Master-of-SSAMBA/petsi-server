9b4c1d0ff6c9159ddd9d9b5e2e28ef18efeeb6b2
양지웅 <yju1028@naver.com>
2024-10-08 10:46:42 +0900

    Deleted backend/notification-service/gradle/wrapper/gradle-wrapper.jar, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/controller/NoticeController.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/dto/request/TokenRequestDto.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/dto/response/DateResponseDto.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/dto/response/NoticeResponseDto.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/entity/Notice.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/entity/UserToken.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/repository/NoticeRepository.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/repository/UserTokenRepository.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/domain/notice/service/NoticeService.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/config/FirebaseConfig.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/config/SwaggerConfig.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/exception/BusinessLogicException.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/exception/ExceptionCode.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/kafka/KafkaConsumer.java, backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/NoticeServiceApplication.java, backend/notification-service/src/main/resources/db/migration/V1__initial_setup.sql, backend/notification-service/src/main/resources/db/migration/V2__add_usertoken_table.sql, backend/notification-service/src/main/resources/application-dev.yml, backend/notification-service/src/main/resources/application-prod.yml, backend/notification-service/src/main/resources/application.yml, backend/notification-service/src/test/java/com/ssamba/petsi/notice_service/NoticeServiceApplicationTests.java, backend/notification-service/.gitignore, backend/notification-service/build.gradle, backend/notification-service/gradlew, backend/notification-service/gradlew.bat, backend/notification-service/settings.gradle

--- backend/notification-service/src/main/java/com/ssamba/petsi/notice_service/global/kafka/KafkaConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.ssamba.petsi.notice_service.global.kafka;
-
-import com.ssamba.petsi.notice_service.domain.notice.dto.response.NoticeResponseDto;
-import org.springframework.kafka.annotation.KafkaListener;
-
-public class KafkaConsumer {
-}
--- backend/notification-service/src/main/resources/db/migration/V2__add_usertoken_table.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS user_token;
-CREATE TABLE user_token (
-    user_token_id bigint NOT NULL AUTO_INCREMENT,
-    user_id bigint NOT NULL,
-    token varchar(255) NOT NULL,
-    created_at timestamp NOT NULL,
-    PRIMARY KEY (user_token_id)
-);
