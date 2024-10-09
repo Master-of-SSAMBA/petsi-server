d53bf92ffbe7f0613f31ec78c7bf239be932f746
조수연 <melthetensai@gmail.com>
2024-10-09 18:07:34 +0900

    Refactor: add pet infos

--- backend/account-service/src/main/java/com/ssamba/petsi/account_service/global/client/PetClient.java
+++ backend/account-service/src/main/java/com/ssamba/petsi/account_service/global/client/PetClient.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.ssamba.petsi.account_service.global.dto.PetCustomDto;
+import com.ssamba.petsi.account_service.global.dto.PetResponseDto;
 
 @FeignClient(name = "pet-service")
 public interface PetClient {
@@ -18,4 +20,6 @@ public interface PetClient {
 	@PostMapping("/api/v1/pet/find-pets-by-pet-id/{userId}")
 	List<PetCustomDto> findPetCustomDtoById(@PathVariable Long userId, @RequestBody List<Long> pets);
 
+	@GetMapping("/api/v1/pet/find-pets-for-user-info/{userId}")
+	List<PetResponseDto> findPetForUserInfo(@PathVariable Long userId);
 }
