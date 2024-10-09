18d6602eb4bff844f3ca98e1a1cfed2334c2f04c
조수연 <melthetensai@gmail.com>
2024-10-09 13:23:33 +0900

    Refactor: account-service 내 kafka 수정

--- backend/account-service/src/main/java/com/ssamba/petsi/account_service/global/kafka/KafkaProducer.java
+++ backend/account-service/src/main/java/com/ssamba/petsi/account_service/global/kafka/KafkaProducer.java
@@ -3,7 +3,11 @@ package com.ssamba.petsi.account_service.global.kafka;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssamba.petsi.account_service.global.dto.NotificationProducerDto;
+import com.ssamba.petsi.account_service.global.exception.BusinessLogicException;
+import com.ssamba.petsi.account_service.global.exception.ExceptionCode;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,10 +15,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class KafkaProducer {
 
-	private final KafkaTemplate<String, NotificationProducerDto> kafkaTemplate;
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final ObjectMapper objectMapper;
 
 	public void send(NotificationProducerDto notificationProducerDto) {
-		kafkaTemplate.send("kafka-topic", notificationProducerDto);
+		try {
+			String json = objectMapper.writeValueAsString(notificationProducerDto);
+			kafkaTemplate.send("kafka-topic", json);
+		} catch (JsonProcessingException e) {
+			throw new BusinessLogicException(ExceptionCode.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 
 }
