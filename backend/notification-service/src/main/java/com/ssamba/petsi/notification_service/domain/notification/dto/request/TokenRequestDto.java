aa9cb91ea939e6f9be642d73887ca8a4e24f451f
조수연 <melthetensai@gmail.com>
2024-10-11 01:25:45 +0900

    hotfix: add import

--- backend/notification-service/src/main/java/com/ssamba/petsi/notification_service/domain/notification/dto/request/TokenRequestDto.java
+++ backend/notification-service/src/main/java/com/ssamba/petsi/notification_service/domain/notification/dto/request/TokenRequestDto.java
@@ -3,6 +3,8 @@ package com.ssamba.petsi.notification_service.domain.notification.dto.request;
 import com.ssamba.petsi.notification_service.domain.notification.entity.UserToken;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
